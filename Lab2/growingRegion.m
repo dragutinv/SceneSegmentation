06c2eb73a7fdba54b38cfccc4f9cf2dceb21af2d
Dragutin Vujovic <dragutin.v@gmail.com>
2015-03-25 17:26:20 +0100

    - GUI for Lab2

---
+++ Lab2/growingRegion.m
@@ -0,0 +1,61 @@
+function [ regions, regionNumber, executionTime] = growingRegion( image, connectivity, threshold)
+%GROWINGREGION Function performs scene segmentation using growing regions
+%algorithm
+
+tic;%measure execution time of region growing
+
+dim = size(image);
+
+%save for each pixel its region
+visitedPixels = zeros(dim(1), dim(2));
+
+%threshold
+regionNumber = 0;
+
+for row = 1:dim(1)
+    for column = 1:dim(2) %for every pixel in the image 
+        if (visitedPixels(row, column) == 0)
+            %%NEW REGION
+            
+            %initial mean intensity, used to decide if neighbour pixel
+            %belongs to the same region
+            meanIntensity = double(image(row, column, :));
+            regionNumber = regionNumber+1;
+            neighbours = [];
+            queue = [row, column];%seed point
+            visitedPixels(queue(1), queue(2)) = regionNumber; 
+            counter = 1;
+            
+            while (isempty(queue) == 0)
+                    %GROW REGION        
+                    neighbours = findNeighbours([queue(1) queue(2)], connectivity,dim);
+                    queue = queue(3:end);%remove seed, add neighbours to queue
+                    
+                    %ADD ALL NEIBOURHOOD PIXELS THAT BELONG TO THE SAME REGION
+                    for i = 1:2:length(neighbours)
+                        
+                        %ADD PIXEL TO REGION IF ITS NOT VISITED AND IF
+                        %SATISFIES BELONGING CRITERIUM
+                        if (visitedPixels(neighbours(i),neighbours(i+1)) == 0) ...
+                            && getDifference(image(neighbours(i),neighbours(i+1), :), meanIntensity) < threshold
+                        
+                            queue = [queue neighbours(i),neighbours(i+1)];
+                            visitedPixels(neighbours(i),neighbours(i+1)) = regionNumber; 
+                            counter = counter + 1;
+                            
+                            %update mean intensity with mean value of all
+                            %pixels in particular region
+                            meanIntensity = getMean(image(neighbours(i),neighbours(i+1),:), meanIntensity, counter);
+                        end
+                    end
+            end
+        end
+    end   
+end
+
+%export labels for each pixel of image
+regions = label2rgb(visitedPixels);
+executionTime = toc; %measure elapsed time of region growing
+
+end
+
--- Lab2/interface.m
+++ Lab2/interface.m
@@ -22,7 +22,7 @@ function varargout = interface(varargin)
 
 % Edit the above text to modify the response to help interface
 
-% Last Modified by GUIDE v2.5 23-Mar-2015 17:27:33
+% Last Modified by GUIDE v2.5 25-Mar-2015 17:22:43
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -100,11 +100,6 @@ if (length(handles.ImgFileName) > 3)
     guidata(interface, handles);
     T = toc; %measure elapsed time    
 end
- 
-
-
-
-
 
 % --- Executes during object creation, after setting all properties.
 function txtWindowSize_CreateFcn(hObject, eventdata, handles)
@@ -127,7 +122,15 @@ function txtWndSize_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of txtWndSize as text
 %        str2double(get(hObject,'String')) returns contents of txtWndSize as a double
+global outputImageContrast;
+global outputImageEnergy;
+global outputImageHomogeneity;
+global outputImageEntropy;
 
+outputImageContrast = [];
+outputImageEnergy = [];
+outputImageHomogeneity = [];
+outputImageEntropy = [];
 
 % --- Executes during object creation, after setting all properties.
 function txtWndSize_CreateFcn(hObject, eventdata, handles)
@@ -170,25 +173,201 @@ function btnPerformSegmentation_Callback(hObject, eventdata, handles)
 % hObject    handle to btnPerformSegmentation (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global outputImageContrast;
+global outputImageEnergy;
+global outputImageHomogeneity;
+global outputImageEntropy;
 
 list = get(handles.lstProps, 'String');
 props = list{get(handles.lstProps, 'value')};
 
-[outputImageContrast, outputImageEnergy, outputImageHomogeneity, outputImageEntropy] = textureSegmentation([handles.ImgPathName,handles.ImgFileName], props, str2num(get(handles.txtWndSize, 'String')));
+if size(outputImageContrast) < 10
+    [outputImageContrast, outputImageEnergy, outputImageHomogeneity, outputImageEntropy] = textureSegmentation([handles.ImgPathName,handles.ImgFileName], props, str2num(get(handles.txtWndSize, 'String')));
+end
 axes(handles.imgPattern);
-imagesc();
-
-set(hObject,'outputImageContrast',outputImageContrast);
-set(hObject,'outputImageEnergy',outputImageEnergy);
-set(hObject,'outputImageHomogeneity',outputImageHomogeneity);
-set(hObject,'outputImageEntropy',outputImageEntropy);
 
 if strcmp(props, 'Contrast') == 1
-    imshow(uint8(outputImageContrast*255));
+    im = uint8(outputImageContrast*255);
 elseif strcmp(props, 'Energy') == 1
-    imshow(uint8(outputImageEnergy*255));
+    im = uint8(outputImageEnergy*255);
 elseif strcmp(props, 'Homogeneity') == 1
-    imshow(uint8(outputImageHomogeneity*255));    
+    im = uint8(outputImageHomogeneity*255);    
 elseif strcmp(props, 'Entropy') == 1
-    imshow(uint8(outputImageEntropy*255));    
+    im = uint8(outputImageEntropy*255);    
+end
+
+imagesc();
+imshow(im);
+
+%perform region growing segmentation
+connectivity = 4;
+connectivityFour = get(handles.rb4Neigbours,'Value');
+if (connectivityFour == 0)
+    connectivity = 8;
+end
+
+threshold = str2double(get(handles.txtThreshold,'String'));
+
+[regions] = growingRegion(im, connectivity, threshold);
+axes(handles.imgGrowingRegion);
+imagesc();
+imshow(regions);
+
+
+% --- Executes on button press in rb4Neigbours.
+function rb4Neigbours_Callback(hObject, eventdata, handles)
+% hObject    handle to rb4Neigbours (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of rb4Neigbours
+set(handles.rb8Neigbours, 'Value', 0);
+
+
+% --- Executes on button press in rb8Neigbours.
+function rb8Neigbours_Callback(hObject, eventdata, handles)
+% hObject    handle to rb8Neigbours (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of rb8Neigbours
+set(handles.rb4Neigbours, 'Value', 0);
+
+
+
+function txtThreshold_Callback(hObject, eventdata, handles)
+% hObject    handle to txtThreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txtThreshold as text
+%        str2double(get(hObject,'String')) returns contents of txtThreshold as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txtThreshold_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txtThreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in chbContrast.
+function chbContrast_Callback(hObject, eventdata, handles)
+% hObject    handle to chbContrast (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of chbContrast
+
+
+% --- Executes on button press in chbEnergy.
+function chbEnergy_Callback(hObject, eventdata, handles)
+% hObject    handle to chbEnergy (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of chbEnergy
+
+
+% --- Executes on button press in chbHomogeneity.
+function chbHomogeneity_Callback(hObject, eventdata, handles)
+% hObject    handle to chbHomogeneity (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of chbHomogeneity
+
+
+% --- Executes on button press in chbEntropy.
+function chbEntropy_Callback(hObject, eventdata, handles)
+% hObject    handle to chbEntropy (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of chbEntropy
+
+
+% --- Executes on button press in btnClassification.
+function btnClassification_Callback(hObject, eventdata, handles)
+% hObject    handle to btnClassification (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+chbContrast = get(handles.chbContrast, 'Value');
+chbEnergy = get(handles.chbEnergy, 'Value');
+chbHomogeneity = get(handles.chbHomogeneity, 'Value');
+chbEntropy = get(handles.chbEntropy, 'Value');
+
+score = scr_classifyFunc([chbContrast chbEnergy chbHomogeneity chbEntropy]);
+set(handles.txtScore, 'String', num2str(score));
+
+
+% --- Executes on button press in btnAllFeatures.
+function btnAllFeatures_Callback(hObject, eventdata, handles)
+% hObject    handle to btnAllFeatures (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global outputImageContrast;
+global outputImageEnergy;
+global outputImageHomogeneity;
+global outputImageEntropy;
+
+if (size(outputImageContrast) > 10)
+    figure('units','normalized','outerposition',[0 0 1 1]);
+    subplot(1, 4, 1); imshow(outputImageContrast); title('Contrast');
+    subplot(1, 4, 2); imshow(outputImageEnergy); title('Energy');
+    subplot(1, 4, 3); imshow(outputImageHomogeneity); title('Homogeneity');
+    subplot(1, 4, 4); imshow(outputImageEntropy); title('Entropy');
 end
